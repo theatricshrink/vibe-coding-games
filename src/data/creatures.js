a5324deb4b38ffbfa227c5575089a4ab62f95205
root <root@KERTHI-PC.localdomain>
2026-03-19 19:42:37 +0100

    feat: add creature definitions

---
+++ src/data/creatures.js
@@ -0,0 +1,10 @@
+var CREATURES = [
+  { id: 'blaettchen', name: 'Blättchen', emoji: '🍃', color: 0x4CAF50, rarity: 'common',   weight: 40 },
+  { id: 'mooskind',   name: 'Mooskind',  emoji: '🟢', color: 0x2E7D32, rarity: 'common',   weight: 40 },
+  { id: 'dornika',    name: 'Dornika',   emoji: '🌵', color: 0x66BB6A, rarity: 'common',   weight: 40 },
+  { id: 'blumsel',    name: 'Blumsel',   emoji: '🌸', color: 0xF48FB1, rarity: 'uncommon', weight: 15 },
+  { id: 'rankbert',   name: 'Rankbert',  emoji: '🐍', color: 0x558B2F, rarity: 'uncommon', weight: 15 },
+  { id: 'pilzling',   name: 'Pilzling',  emoji: '🍄', color: 0xE53935, rarity: 'uncommon', weight: 15 },
+  { id: 'baumgeist',  name: 'Baumgeist', emoji: '🌳', color: 0x8D6E63, rarity: 'rare',     weight: 5  },
+  { id: 'bisaknosp',  name: 'Bisaknosp', emoji: '🌱', color: 0x26A69A, rarity: 'rare',     weight: 5  }
+];
